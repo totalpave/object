dca9ba1a58d1292df08462148f980a4c6dd9ef8b
Norman Breau <norman@normanbreau.com>
2020-09-03 15:45:26 -0300

    fix: KeyValueAdapter error if given an incorrect key

--- spec/KeyValueAdapter.spec.ts
+++ spec/KeyValueAdapter.spec.ts
@@ -16,4 +16,20 @@ describe('KeyValueAdapter', () => {
         expect(adapter.adapt('test')).toBe('hi');
         expect(adapter.adapt('value')).toBe('world');
     });
+
+    it('should error', () => {
+        interface ITestMap {
+            test: string;
+            value: string;
+        }
+
+        let adapter: KeyValueAdapter<ITestMap> = new KeyValueAdapter({
+            test: "hi",
+            value: "world"
+        });
+
+        expect(() => {
+            adapter.adapt(<any>"asdf");
+        }).toThrowError(/doesn't exist in map\.$/);
+    });
 });
