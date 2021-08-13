3a2c24b03d79c720a117f65df9d8152bc5c6f86a
Norman Breau <norman@normanbreau.com>
2021-08-13 17:32:26 +0000

    eslint upgrade

--- src/KeyValueAdapter.ts
+++ src/KeyValueAdapter.ts
@@ -1,25 +1,25 @@
 import { IDictionary } from '@totalpave/interfaces';
 
 export class KeyValueAdapter<TMap extends IDictionary = IDictionary> {
-    private _map: TMap;
+    private $map: TMap;
 
     public constructor(adapterMap: TMap) {
         this.setAdapterMap(adapterMap);
     }
 
     public setAdapterMap(adapterMap: TMap): void {
-        this._map = adapterMap;
+        this.$map = adapterMap;
     }
 
     public setAdapterKey(key: keyof TMap, value: TMap[keyof TMap]): void {
-        this._map[key] = value;
+        this.$map[key] = value;
     }
 
     public adapt(key: keyof TMap): TMap[keyof TMap] {
-        if (Object.keys(this._map).indexOf(<string>key) === -1) {
+        if (Object.keys(this.$map).indexOf(<string>key) === -1) {
             throw new Error(`Key "${key}" doesn't exist in map.`);
         }
         
-        return this._map[key];
+        return this.$map[key];
     }
 }
